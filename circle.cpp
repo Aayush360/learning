3082bb031601e1c8e8d06481008b4c0ea9cffda2
Aayush padel <paudel.aayush@ymail.com>
2018-06-19 09:38:32 +0545

    adding circle program

---
+++ circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define pi 3.14
+
+int main()
+{
+	int r;
+	float area;
+	std::cout<<"enter the radius";
+	std::cin>>r;
+	area=pi*r*r;
+    std::cout<<"the area is:"<<"\t"<<area;
+	return 0;
+	
+	
+}
